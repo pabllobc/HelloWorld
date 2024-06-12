9396212b387e394de3e9473ebe8ddd16e54d59fe
Pabllo Borges Cardoso <pabllobc@gmail.com>
2024-06-12 13:42:50 -0300

    Segundo commit

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+	var n1 float32
+	var n2 float32
+
+	n1 = 100
+	n2 = 220
+	soma := n1 + n2
+
+	fmt.Fprintln(os.Stdout, []any{"Soma = %f", soma}...)
+
 	fmt.Println("Hello World!!!")
 }
